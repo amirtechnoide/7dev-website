92a7e05e4c95384033d0c0cf1884b3be29525262
amirtechnoide <>
2020-07-24 20:15:58 +0100

    version 1 released with btn on mobile

---
+++ js/counter.js
@@ -0,0 +1,26 @@
+var counters = document.querySelectorAll('.counter');
+let speed = 200000;
+
+counters.forEach(counter => {
+    let updateCount = () => {
+        let target = counter.getAttribute('data-target');
+        let count = +counter.innerHTML;
+
+        let inc = target / speed;
+        
+        if(count < target) {
+            counter.innerText = Math.ceil(count + inc);
+            setTimeout(updateCount, );
+        }else {
+            count.innerText = `${target}+`
+        }
+        // console.log( target);
+    }
+    
+    // if(target = 150){
+    //     let speed1 = 1000;
+    // } else if(target = 20) {
+    //     let speed2 = 8000;
+    // }
+    updateCount();
+});
